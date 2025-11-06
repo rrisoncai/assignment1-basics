a5b9663cefb81e2b249db9ef0d5758a8b1b443bd
rrisoncai <lotilas@hotmail.com>
2025-11-06 16:58:20 +0800

    Generate token list from owt_train, and avoid OOM.

--- cs336_basics/BPETokenizer.py
+++ cs336_basics/BPETokenizer.py
@@ -189,29 +189,92 @@ class BPETokenizer:
 
         return byte_seq.decode("utf-8", errors="replace")
     
+import os
+from typing import BinaryIO
+
+
+def find_chunk_boundaries(
+    file: BinaryIO,
+    desired_num_chunks: int,
+    split_special_token: bytes,
+) -> list[int]:
+    """
+    Chunk the file into parts that can be counted independently.
+    May return fewer chunks if the boundaries end up overlapping.
+    """
+    assert isinstance(split_special_token, bytes), "Must represent special token as a bytestring"
+
+    # Get total file size in bytes
+    file.seek(0, os.SEEK_END)
+    file_size = file.tell()
+    file.seek(0)
+
+    chunk_size = file_size // desired_num_chunks
+
+    # Initial guesses for chunk boundary locations, uniformly spaced
+    # Chunks start on previous index, don't include last index
+    chunk_boundaries = [i * chunk_size for i in range(desired_num_chunks + 1)]
+    chunk_boundaries[-1] = file_size
+
+    mini_chunk_size = 4096  # Read ahead by 4k bytes at a time
+
+    for bi in range(1, len(chunk_boundaries) - 1):
+        initial_position = chunk_boundaries[bi]
+        file.seek(initial_position)  # Start at boundary guess
+        while True:
+            mini_chunk = file.read(mini_chunk_size)  # Read a mini chunk
+
+            # If EOF, this boundary should be at the end of the file
+            if mini_chunk == b"":
+                chunk_boundaries[bi] = file_size
+                break
+
+            # Find the special token in the mini chunk
+            found_at = mini_chunk.find(split_special_token)
+            if found_at != -1:
+                chunk_boundaries[bi] = initial_position + found_at
+                break
+            initial_position += mini_chunk_size
+
+    # Make sure all boundaries are unique, but might be fewer than desired_num_chunks
+    return sorted(set(chunk_boundaries))
+
 if __name__ == "__main__":
-    input_file = "../../data/TinyStoriesV2-GPT4-train.txt"
-    # input_file = "../../data/owt_valid.txt"
+    # input_file = "../../data/TinyStoriesV2-GPT4-train.txt"
+    input_file = "../../data/owt_train.txt"
     lines = []
-    with open(input_file, "r", encoding="utf-8") as f:
-        text = f.read()
+
 
     bpe = BPETokenizer.from_files(
-        vocab_filepath="../../data/bpe_artifacts_TinyStoriesV2-GPT4-train/vocab.hex.json",
-        merges_filepath="../../data/bpe_artifacts_TinyStoriesV2-GPT4-train/merges.hex.json",
-        # vocab_filepath="../../data/bpe_artifacts_owt_train/vocab.hex.json",
-        # merges_filepath="../../data/bpe_artifacts_owt_train/merges.hex.json",
-        special_tokens=["<|endoftext|>"]
+        # vocab_filepath="../../data/bpe_artifacts_TinyStoriesV2-GPT4-train/vocab.hex.json",
+        # merges_filepath="../../data/bpe_artifacts_TinyStoriesV2-GPT4-train/merges.hex.json",
+        vocab_filepath="../../data/bpe_artifacts_owt_train/vocab.hex.json",
+        merges_filepath="../../data/bpe_artifacts_owt_train/merges.hex.json",
+        # special_tokens=["<|endoftext|>"]
         )
-    tik = time.time()
-    token_list = bpe.encode(text)
-    tok = time.time()
-    elapsed = tok - tik
-    bytes_of_text = len(text.encode("utf-8"))
-    print(f"Thoughput is {bytes_of_text / elapsed / 1024:.2f} kB/sec")
-    print("text length:", bytes_of_text)
-    print("token lenght:", len(token_list))
-    print(f"compression ratio: {bytes_of_text / len(token_list):.2f}")
+    token_list = []
+    with open(input_file, "rb") as f:
+        num_processes = 16
+        boundaries = find_chunk_boundaries(f, num_processes, b"<|endoftext|>")
+        num_chunks = len(boundaries) - 1
+
+        # The following is a serial implementation, but you can parallelize this
+        # by sending each start/end pair to a set of processes.
+        for start, end in zip(boundaries[:-1], boundaries[1:]):
+            f.seek(start)
+            chunk = f.read(end - start).decode("utf-8", errors="ignore")
+            # Run pre-tokenization on your chunk and store the counts for each pre-token
+            text = chunk
+            tik = time.time()
+            token_chunk = bpe.encode(text)
+            tok = time.time()
+            token_list.extend(token_chunk)
+            elapsed = tok - tik
+            bytes_of_text = len(text.encode("utf-8"))
+            print(f"Thoughput is {bytes_of_text / elapsed / 1024 / 1024:.2f} MB/sec")
+            print("texts length:", bytes_of_text)
+            print("token length:", len(token_chunk))
+            print(f"compression ratio: {bytes_of_text / len(token_chunk):.2f}")
     import numpy as np
     os.makedirs("../../data/token_ids", exist_ok=True)
     out_path = os.path.join("../../data/token_ids", os.path.basename(input_file) + ".npy")
